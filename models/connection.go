66389a19cca56586410542232ec4a793b221528d
Joel Tinx <wizard.joel@gmail.com>
2017-05-19 22:03:27 -0500

    corrections in file connections

--- models/connection.go
+++ models/connection.go
@@ -1,9 +1,30 @@
 package models
 
 import (
-    "sql"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func getConnnection() {
-    return
+const (
+	dbHost = "localhost"
+	dbName = "dbGemace"
+	dbUser = "root"
+	dbPass =  "lca2012"//"123456"
+	dbPort = 3306
+)
+
+var connsql string
+
+func init() {
+	connsql = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+}
+
+func getConnection() (*sql.DB, error) {
+	db, err := sql.Open("mysql", connsql)
+	if err != nil {
+		panic(err)
+	}
+	return db, err
 }
